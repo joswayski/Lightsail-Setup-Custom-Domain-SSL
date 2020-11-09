42401bfc2f0652798927b91f9c9c4ef6aa4c7a34
Jose Valerio <contact@joswayski.com>
2020-11-08 21:52:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use();
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to my server! This is the homepage on port ${port}`);
+});
+
+app.get("/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
